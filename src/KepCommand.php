63a157a5ef749861325252bc07da35ef69e2bdee
Matuzalém S. Teles <matuzalemteles@gmail.com>
2016-10-13 13:37:15 -0300

    first commit

---
+++ src/KepCommand.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Kep\Installer\Console;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class KepCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('new')
+            ->setDescription('Creat a new Kep application')
+            ->addArgument('name', InputArgument::OPTIONAL);
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $this->verifyApplicationDoesntExist(
+            $directory = ($input->getArgument('name')) ? getcwd().'/'.$input->getArgument('name') : getcwd(),
+            $output
+        );
+
+        $output->writeln('<info>Crafting application...</info>');
+
+        $composer = $this->findComposer();
+
+        $commands = [
+            'mkdir '.$input->getArgument('name'),
+            'cd '.$input->getArgument('name'),
+            'mkdir controllers',
+            'mkdir models',
+            'mkdir seeds',
+            'cd ..',
+            $composer.' require getkep/kep 0.3.2'
+        ];
+
+        $process = new Process(implode(' && ', $commands), $directory, null, null, null);
+
+        $process->run(function ($type, $line) use ($output) {
+           $output->write($line);
+        });
+
+        $output->writeln('<comment>Application ready! Start scaling APIs.</comment>');
+    }
+
+    protected function verifyApplicationDoesntExist($directory, OutputInterface $output)
+    {
+        if ((is_dir($directory) || is_file($directory)) && $directory != getcwd()) {
+            throw new RuntimeException('Application already exists!');
+        }
+
+        $output->writeln($directory);
+    }
+
+    protected function findComposer()
+    {
+        if (file_exists(getcwd().'/composer.phar')) {
+            return '"'.PHP_BINARY.'" composer.phar';
+        }
+        return 'composer';
+    }
+}
